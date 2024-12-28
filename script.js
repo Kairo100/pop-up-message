0672503057f33f714feffc2b2caf53e265d2a6e8
kairo100 <kaironaser100@gmail.com>
2024-12-28 05:55:58 +0300

    this pop up message built using html , css and js

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleNewsLetter()
+{
+    const newsletter = document.getElementById("newsletter");
+    newsletter.classList.toggle("active");
+
+
+}
+
+
+document.getElementById("toggleNewsLetters").onclick=toggleNewsLetter();
+
+//firts js program
+//toggle means to cahnge what is it if it is open close , if it
+// close open it
+// active means class of css to change happened to toggle
+//doc.elbyid calls newsletter ...
+/** */
